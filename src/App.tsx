2c4b5e7e734d3788c81822dae444b3fb9bd2ebda
flightoffancy-b <petuhov.ilja2015@yandex.ru>
2024-04-17 15:22:29 +0400

    init

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <main className="bg-slate-200 w-screen h-screen justify-center items-center flex">
+      <div className="bg-white w-3/5 h-3/5 rounded-3xl">
+        <div className="h-1/5 justify-center items-center flex border-b-2 border-slate-200">
+          <h1 className="text-xl font-semibold">Игра в города на время</h1>
+        </div>
+      </div>
+    </main>
+  )
+}
+
+export default App
